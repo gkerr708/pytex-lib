a57111e04be9d77e8eab2cdaa4b3e9eb71454626
gkerr_laptop <gavin.kerr@dal.ca>
2025-12-02 17:20:37 -0400

    converted to uv

--- tests/test.py
+++ tests/test.py
@@ -2,24 +2,14 @@ from pathlib import Path
 from pytex_lib import write_to_latex
 
 
-@write_to_latex
+@write_to_latex(file_path=Path(__file__).parent / 'test.tex', keyword="keyword in latex doc")
 def get_output(answer: str):
     return answer
 
 if __name__ == "__main__":
 
-    # Path to latex doc
-    file_path = Path(__file__).parent / 'test.tex'
-
-    # Keyword
-    keyword = "keyword in latex doc"
-
     # Answer to be written to the latex doc
     answer = "Response from python script1234."
 
     # Call the test function 
-    get_output(
-        answer=answer, 
-        file_path=file_path, 
-        keyword=keyword
-    )
+    get_output(answer)
